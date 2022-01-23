99af4bd030d801a2aeb6464642c5c9cc1ca9a81b
Andrico <andrico.karoulla@healthhero.com>
2022-01-23 18:14:28 +0000

    refactor(site): separate site from monorepo

--- site/.eleventy.js
+++ site/.eleventy.js
@@ -4,12 +4,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("../packages");
 
   eleventyConfig.on("eleventy.beforeWatch", async (changedFiles) => {
-    const file = await fs.readFile(
-      "../node_modules/@a2000/styles/a2k-styles.css"
-    );
-
     fs.copyFile(
-      "../node_modules/@a2000/styles/a2k-styles.css",
+      "./node_modules/@a2000/styles/a2k-styles.css",
       "./src/a2k-styles.css"
     );
   });
