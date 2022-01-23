89ef695422f234f4bc26105193cdc39298822af0
Andrico <andrico.karoulla@healthhero.com>
2022-01-23 21:36:26 +0000

    fix(site): aim to fix deploy site

--- site/rollup.config.js
+++ site/rollup.config.js
@@ -2,6 +2,6 @@ import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
 
 export default {
   input: "index.html",
-  output: { dir: "dist" },
-  plugins: [html()]
+  output: { dir: "dist", format: "es" },
+  plugins: [html()],
 };
