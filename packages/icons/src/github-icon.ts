d61fc15e366a38d51ff73eca8a027be25d60dd2c
Andrico <andrico.karoulla@healthhero.com>
2022-02-27 21:50:34 +0000

    feat(icons): add github icons

---
+++ packages/icons/src/github-icon.ts
@@ -0,0 +1,3 @@
+import { GithubIcon } from "./GithubIcon";
+
+window.customElements.define("a2k-github-icon", GithubIcon);
--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,5 +1,6 @@
 export { HelpLogo } from "./HelpLogo";
 export { NetworkIcon } from "./NetworkIcon";
 export { DocumentsIcon } from "./DocumentsIcon";
+export { GithubIcon } from "./GithubIcon";
 export { TwitterLogo } from "./TwitterLogo";
 export { WindowsLogo } from "./WindowsLogo";
