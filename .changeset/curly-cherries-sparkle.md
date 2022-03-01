99a0dcbe6986b3ff8245d314954d20175bd8debf
Andrico <andrico.karoulla@healthhero.com>
2022-03-01 22:18:45 +0000

    feat(button): update button styles

---
+++ .changeset/curly-cherries-sparkle.md
@@ -0,0 +1,6 @@
+---
+"@a2000/button": patch
+"@a2000/styles": patch
+---
+
+update button styles
--- packages/button/src/A2kButton.ts
+++ packages/button/src/A2kButton.ts
@@ -3,6 +3,15 @@ import { html, css, LitElement } from "lit";
 export class A2kButton extends LitElement {
   static styles = css`
     #button {
+      background-color: var(--button-color-background);
+      font-family: var(--font-primary);
+      padding: var(--button-padding);
+      font-weight: var(--button-font-weight);
+      box-sizing: border-box;
+      box-shadow: var(--button-shadow);
+      border-width: var(--button-border-width);
+      border-color: var(--button-border-color);
+      border-style: var(--button-border-style);
     }
   `;
 
