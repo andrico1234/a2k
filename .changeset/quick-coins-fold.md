e8a388c6fb3955c4cd8dbc00bf87157a160a0024
Andrico <andrico.karoulla@healthhero.com>
2022-07-15 15:40:24 +0100

    fix(desktop): icons wrap based on desktop height

---
+++ .changeset/quick-coins-fold.md
@@ -0,0 +1,5 @@
+---
+"@a2000/desktop": patch
+---
+
+icons wrap based on desktop height on load
--- packages/desktop/src/A2kDesktop.ts
+++ packages/desktop/src/A2kDesktop.ts
@@ -1,15 +1,37 @@
 import { html, css, LitElement } from "lit";
+import { property, query } from "lit/decorators.js";
+import { styleMap } from "lit/directives/style-map.js";
 
 export class A2kDesktop extends LitElement {
   static styles = css`
     #desktop {
       height: 100%;
     }
+
+    #desktop > slot {
+      display: flex;
+      flex-flow: column wrap;
+      width: fit-content;
+    }
   `;
+
+  @property()
+  slotHeightStyles = {
+    height: "0px",
+  };
+
+  @query("#desktop")
+  desktopContainer!: HTMLSlotElement;
+
+  protected firstUpdated(): void {
+    const containerHeight = getComputedStyle(this.desktopContainer).height;
+
+    this.slotHeightStyles = { height: containerHeight };
+  }
 
   render() {
     return html`<div id="desktop">
-      <slot />
+      <slot id="slot" style=${styleMap(this.slotHeightStyles)}></slot>
     </div>`;
   }
 }
