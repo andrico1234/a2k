946f32caf548c41f6736aa80855d331fbad9b5e2
Andrico <andrico.karoulla@healthhero.com>
2022-10-09 17:09:23 +0100

    fix(core): dedupe dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,8 +9,24 @@ module.exports = {
     "@storybook/addon-essentials",
     "storybook-anima",
   ],
+  // Webpack 4 didn't recognise package.json exports well
+  core: {
+    builder: "webpack5",
+  },
   framework: "@storybook/web-components",
   features: {
     storyStoreV7: true,
+  },
+  // Due to a2k-icons. Static assets are relative to the root, so we need to
+  // let Storybook now where are our static assets are.
+  staticDirs: ["../"],
+  // To allow webpack to import svgs
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
--- packages/desktop/src/A2kDesktop.stories.ts
+++ packages/desktop/src/A2kDesktop.stories.ts
@@ -4,14 +4,21 @@ import "./a2k-desktop";
 import "./a2k-desktop-icon";
 
 const Template: Story = (args) => {
-  return html`<a2k-desktop>
-    <a2k-desktop-icon icon="documents-icon" text="Portfolio"></a2k-desktop-icon>
-    <a2k-desktop-icon
-      icon="network-icon"
-      text="Send Feedback"
-    ></a2k-desktop-icon>
-    <a2k-desktop-icon icon="help-icon" text="About"></a2k-desktop-icon>
-  </a2k-desktop>`;
+  return html`
+    <div style="height:100vh;background:var(--desktop-background)">
+      <a2k-desktop>
+        <a2k-desktop-icon
+          icon="documents-icon"
+          text="Portfolio"
+        ></a2k-desktop-icon>
+        <a2k-desktop-icon
+          icon="network-icon"
+          text="Send Feedback"
+        ></a2k-desktop-icon>
+        <a2k-desktop-icon icon="help-icon" text="About"></a2k-desktop-icon>
+      </a2k-desktop>
+    </div>
+  `;
 };
 
 export const Default = Template.bind({});
