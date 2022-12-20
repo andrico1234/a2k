925a95e34b35d47968c7da964f2a9c422c64f198
Andrico <karoulla.andrico@gmail.com>
2022-12-20 08:08:54 +0000

    feat(site): finish advert functionality

--- site/src/templates/advert.ts
+++ site/src/templates/advert.ts
@@ -1,23 +1,90 @@
 import { html, css, LitElement } from "lit";
-import { property, customElement } from "lit/decorators.js";
+import { property, customElement, query } from "lit/decorators.js";
+import { styleMap } from "lit/directives/style-map.js";
 
-// I want this element to pop up inside of the parent element at a random position, any where between 1 - 20 seconds after parent element exists
-
-// The UI element
-// - absolutely positioned element
+function getRandomNumber(max: number) {
+  return Math.ceil(Math.random() * max);
+}
 
 @customElement("a2k-advert")
 export class Advert extends LitElement {
   static styles = css`
     #wrapper {
       position: absolute;
-      top: 0;
-      left: 0;
-      border: 1px solid red;
+      width: 200px;
+      height: 200px;
     }
   `;
 
+  @property()
+  styles = { top: "0", left: "0" };
+
+  @property()
+  imgSrc = "";
+
+  @property()
+  alt = "";
+
+  @query("#wrapper")
+  wrapperEl!: HTMLDivElement;
+
+  displayDelay: number;
+
+  delayTimeout = null;
+
+  constructor() {
+    super();
+
+    this.displayDelay = getRandomNumber(5) * 1000;
+    this.setupAppearance();
+  }
+
+  private setupAppearance() {
+    setTimeout(() => {
+      this.triggerAppearance();
+      this.setElPosition();
+    }, this.displayDelay);
+  }
+
+  private triggerAppearance() {
+    this.wrapperEl.removeAttribute("hidden");
+  }
+
+  private setElPosition() {
+    const [maxX, maxY] = this.calculateValidPositionRange();
+
+    const xPos = getRandomNumber(maxX);
+    const yPos = getRandomNumber(maxY);
+
+    this.styles = {
+      left: `${xPos}px`,
+      top: `${yPos}px`,
+    };
+  }
+
+  private calculateValidPositionRange(): [number, number] {
+    const { offsetParent } = this.wrapperEl;
+    const { width, height } = this.wrapperEl.getBoundingClientRect();
+    const windowDimensions = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    const { width: parentsWidth, height: parentsHeight } =
+      offsetParent?.getBoundingClientRect() ?? windowDimensions;
+
+    const xDiff = parentsWidth - width;
+    const maxX = xDiff > 0 ? xDiff : 0;
+
+    const yDiff = parentsHeight - height;
+    const maxY = yDiff > 0 ? yDiff : 0;
+
+    return [maxX, maxY];
+  }
+
   render() {
-    return html`<div id="wrapper"><p>Hello there</p></div>`;
+    return html`<div style=${styleMap(this.styles)} hidden id="wrapper">
+      <img src=${this.imgSrc} alt=${this.alt} />
+    </div>`;
   }
 }
--- site/src/templates/ie5.js
+++ site/src/templates/ie5.js
@@ -1,11 +1,10 @@
 import { css, html, LitElement } from "lit";
 import "./advert";
 
-const advert1 = new URL("../images/cookie-2.png", import.meta.url).href;
-const advert2 = new URL("../images/cookie-2.png", import.meta.url).href;
-const advert3 = new URL("../images/cookie-2.png", import.meta.url).href;
-const advert4 = new URL("../images/cookie-2.png", import.meta.url).href;
-
+const advert1 = new URL("../images/image-1.jpg", import.meta.url).href;
+const advert2 = new URL("../images/image-1.jpg", import.meta.url).href;
+const advert3 = new URL("../images/image-1.jpg", import.meta.url).href;
+const advert4 = new URL("../images/image-1.jpg", import.meta.url).href;
 class IE5 extends LitElement {
   static styles = css`
     :host {
@@ -24,20 +23,20 @@ class IE5 extends LitElement {
 
     #content {
       position: relative;
-      padding: 0;
     }
 
     a2k-stack {
       position: relative;
     }
 
-    a2k-window > a2k-marquee {
+    a2k-marquee {
       padding: 0;
     }
 
     img {
       object-fit: cover;
       padding: 0;
+      max-width: 100%;
     }
   `;
 
@@ -68,7 +67,7 @@ class IE5 extends LitElement {
   }
 
   render() {
-    return html` <a2k-window draggable closeable heading="Internet Explorer 5">
+    return html`<a2k-window draggable closeable heading="Internet Explorer 5">
       <a2k-window-toolbar slot="toolbar">
         <a2k-window-toolbar-item>
           <div class="toolbar-row">
@@ -79,20 +78,23 @@ class IE5 extends LitElement {
           </div>
         </a2k-window-toolbar-item>
       </a2k-window-toolbar>
-      <a2k-stack>
-        <a2k-marquee>
-          <p slot="text">
-            Internet Explorer has reached end of life. RIP king.
-          </p>
-        </a2k-marquee>
+
+      <a2k-marquee>
+        <p slot="text">Internet Explorer has reached end of life. RIP king.</p>
+      </a2k-marquee>
+      <div id="content">
         <img src="/images/ie5-splash.png" />
-        <a2k-advert></a2k-advert>
-        <a2k-window-status-bar
-          statusOne="Loaded"
-          statusTwo="${this.connection}"
-          slot="statusbar"
-        ></a2k-window-status-bar>
-      </a2k-stack>
+        <a2k-advert alt="hey" imgSrc=${advert1}></a2k-advert>
+        <a2k-advert alt="hey" imgSrc=${advert2}></a2k-advert>
+        <a2k-advert alt="hey" imgSrc=${advert3}></a2k-advert>
+        <a2k-advert alt="hey" imgSrc=${advert4}></a2k-advert>
+      </div>
+
+      <a2k-window-status-bar
+        statusOne="Loaded"
+        statusTwo="${this.connection}"
+        slot="statusbar"
+      ></a2k-window-status-bar>
     </a2k-window>`;
   }
 }
